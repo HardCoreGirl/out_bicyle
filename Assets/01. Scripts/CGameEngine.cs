b5b9dd27ff5eb5d96b52adb7126208f6eb1158a3
HardCoreGandhi <feardoll@gmail.com>
2022-04-26 22:36:04 +0900

    기본 로직 개발

--- Assets/01. Scripts/CGameEngine.cs
+++ Assets/01. Scripts/CGameEngine.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class CGameEngine : MonoBehaviour
 {
+    public GameObject m_goPlayer;
+
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = m_goPlayer.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.AddForce(Vector2.right);
     }
 }
